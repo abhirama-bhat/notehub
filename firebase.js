e754d4df5d28598eb5b7bccb6f1ce9dbd10e83e9
abhirama-bhat <abhiramabhat27@gmail.com>
2025-11-27 10:25:58 +0530

    fixed firebase.js export

--- firebase.js
+++ firebase.js
@@ -1,3 +1,4 @@
+// --- Firebase Config ---
 const firebaseConfig = {
   apiKey: "AIzaSyDKt2SwZ8huxz9kmXhqkvHTQWRwnE8MUO8",
   authDomain: "studyhub-519c3.firebaseapp.com",
@@ -7,8 +8,19 @@ const firebaseConfig = {
   appId: "1:154821601505:web:84012068aba30ada806fa0",
   measurementId: "G-YG6H2PEP1G"
 };
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+// IMPORTS
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+// --- FIXED FUNCTION ---
+export function initFirebase() {
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  // expose globally for other modules
+  window.firebaseDB = db;
+
+  return { app, db };
+}
+

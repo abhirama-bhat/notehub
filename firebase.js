8017a2ba52b88e7593c063f381309dcddc79b5d8
abhirama-bhat <abhiramabhat27@gmail.com>
2025-11-27 15:02:09 +0530

    fixed firebase exports finally

--- firebase.js
+++ firebase.js
@@ -1,17 +1,20 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDKt2SwZ8huxz9kmXhqkvHTQWRwnE8MUO8",
-  authDomain: "studyhub-519c3.firebaseapp.com",
-  projectId: "studyhub-519c3",
-  storageBucket: "studyhub-519c3.firebasestorage.app",
-  messagingSenderId: "154821601505",
-  appId: "1:154821601505:web:84012068aba30ada806fa0",
-  measurementId: "G-YG6H2PEP1G"
-};
+// -------- Firebase Config + Initialization --------
 
-import { initializeApp }
-  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore }
-  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+// These will be filled when initFirebase() is called
+let app = null;
+let db = null;
+
+// Called by upload.html, notes.html, admin.html
+export function initFirebase(config) {
+  if (!app) {
+    app = initializeApp(config);
+    db = getFirestore(app);
+    console.log("Firebase initialized");
+  }
+}
+
+// Export db so pages can use it
+export { db };

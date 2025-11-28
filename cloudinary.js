9b4041796dddad0def91f72785d335bcaed8e703
abhirama-bhat <abhiramabhat27@gmail.com>
2025-11-28 17:03:06 +0530

    Updated Cloudinary code + fixes

--- cloudinary.js
+++ cloudinary.js
@@ -1,13 +1,13 @@
 export async function uploadToCloudinary(file) {
   const cloudName = "duluosq5w";
-  const preset = "notehub_preset";
+  const preset = "notehub_unsigned_pdf"; // NEW PRESET NAME
 
   const fd = new FormData();
   fd.append("file", file);
   fd.append("upload_preset", preset);
 
   const res = await fetch(
-    `https://api.cloudinary.com/v1_1/${cloudName}/auto/upload`,
+    `https://api.cloudinary.com/v1_1/${cloudName}/raw/upload`,
     { method: "POST", body: fd }
   );
 
